2916978b05868c9b10626fcd356fc9678b5ef31c
Daniel Zanoni <danielzanoni2001@gmail.com>
2026-07-16 19:34:31 -0300

    feat: implemented ChatHub

---
+++ Chat.OhHell/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.OhHell.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinLobby(string lobbyId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, lobbyId);
+        }
+
+        public async Task LeaveLobby(string lobbyId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, lobbyId);
+        }
+
+        public async Task SendMessage(string lobbyid, string user, string message)
+        {
+            await Clients
+                .Group(lobbyid)
+                .SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
